a5872a509490209fa2e153f96f2037b7330d7212
zhangyz1202 <a0968036320@gmail.com>
2025-02-21 12:09:26 +0800

    week01-1

---
+++ week01-1.cpp
@@ -0,0 +1,12 @@
+///week01-1.cpp 要自己打好全部的檔名,後面要打.cpp
+///在瘋狂程設(練習模式)寫好的程式碼,全選,Ctrl-C
+///CodeBlocks File-New-Empty 空白檔案,Ctrl-s存檔
+#include <stdio.h>///使用 standard 標準的input/output
+
+int main()
+{
+    int a,b; ///整數 a,b
+    scanf("%d%d" ,&a,&b); ///讀入資料(小心 "%d%d 後面「逗號」
+    ///兩個%d%d  & &
+    printf("%d" , a + b); ///印出資料,把答案印出來
+}
