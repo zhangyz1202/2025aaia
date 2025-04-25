d628df3317912a326aa1d823a5f2c157e40f223a
zhangyz1202 <a0968036320@gmail.com>
2025-04-25 11:54:53 +0800

    Add files via upload

---
+++ week10/happy/week10_1_bubble_happy_part1/week10_1_bubble_happy_part1.pde
@@ -0,0 +1,2 @@
+// week10_1_bubble_happy_part1
+Plmage happy1. happy2;
---
+++ week10/week10-2.cpp
@@ -0,0 +1,11 @@
+ ///week10-2.cpp
+#include <stdio.h>
+int main()
+{
+    printf("叫块1俱计");
+    int n;
+    scanf("%d", &n);
+    printf("计琌 %d\n", n %10);
+    printf("计琌 %d\n", n/10 %10);
+    printf("κ计琌 %d\n", n/100 %10);
+}
